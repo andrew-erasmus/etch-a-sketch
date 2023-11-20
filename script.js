df67582379c4d73d068655c73db6cd2cc58d88b2
andrew-erasmus <andrew.r.eras@gmail.com>
2023-11-20 13:47:30 +0200

    Add dynamic sizing of pixels

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // Create the sketch squares
 
-let container = document.getElementById('sketch-container');  
+let container = document.getElementById('sketch-container');    
+var computedStyles = window.getComputedStyle(container);
+
+const height = computedStyles.getPropertyValue('height');
+const width = computedStyles.getPropertyValue('width');
+
+var numericHeight = parseFloat(height);
+var numericWidth = parseFloat(width);
+
+
+let squareH = numericHeight/16;
+let squareW = numericWidth/16;
 
 for(let i=0; i<256;i++){
     let square = document.createElement('div');
     square.classList.add("square");
+    square.style.width = squareW + 'px';
+    square.style.height = squareH + 'px';
     container.appendChild(square);
-
 }
+
