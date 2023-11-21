789c94a394469c0cec0c13b4c1fab48877be9a82
andrew-erasmus <andrew.r.eras@gmail.com>
2023-11-21 14:49:31 +0200

    Add pixel size slider - final commit

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
-// Create the sketch squares
+//Determine dimensions
+let slider = document.getElementById("pixel-dimensions");
+let output = document.getElementById("dimensions");
 
-let container = document.getElementById('sketch-container');
-var computedStyles = window.getComputedStyle(container);
+let pixelDimensions = slider.value;
+output.innerHTML = slider.value + ' x ' + slider.value; // Display the default slider value
+
+// Create the sketch squares
+const container = document.getElementById('sketch-container');
+const computedStyles = window.getComputedStyle(container);
 const height = computedStyles.getPropertyValue('height');
 const width = computedStyles.getPropertyValue('width');
-var numericHeight = parseFloat(height);
-var numericWidth = parseFloat(width);
-let squareH = numericHeight / 16;
-let squareW = numericWidth / 16;
-
+const numericHeight = parseFloat(height);
+const numericWidth = parseFloat(width);
+let squareH = numericHeight / pixelDimensions;
+let squareW = numericWidth / pixelDimensions;
+//Color of paint
 let currentColor = 'black';
 const colorPicker = document.getElementById('color-picker');
 colorPicker.addEventListener("input", watchColorPicker, false);
 colorPicker.addEventListener("change", watchColorPicker, false);
-
-
-loadCanvas();
-
+//Dragging functionality
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
+loadCanvas();
+
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    pixelDimensions=this.value;
+    squareH = numericHeight / pixelDimensions;
+    squareW = numericWidth / pixelDimensions;
+    output.innerHTML = this.value + ' x ' + this.value;
+    redrawCanvas(pixelDimensions);
+}
 
 function loadCanvas() {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < pixelDimensions * pixelDimensions; i++) {
         let square = document.createElement('div');
         square.classList.add("square");
         square.id = i;
@@ -40,6 +54,31 @@ function loadCanvas() {
 
 
     }
+}
+
+function redrawCanvas(pixelDimensions){
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        container.removeChild(square);
+    });
+
+    for (let i = 0; i < pixelDimensions * pixelDimensions; i++) {
+        let square = document.createElement('div');
+        square.classList.add("square");
+        square.id = i;
+        square.style.width = squareW + 'px';
+        square.style.height = squareH + 'px';
+
+        container.appendChild(square);
+        square.addEventListener('dragstart', function (event) {
+            // Prevent the default drag-and-drop behavior
+            event.preventDefault();
+        });
+        square.addEventListener('mousedown', drawInColor);
+        square.addEventListener('mouseover', drawInColor);
+    }
+
 }
 
 //Color picker changer
