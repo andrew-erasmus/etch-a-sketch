9a71c56bb6bbb3b6c4bf84d9f78914f2157d23e3
andrew-erasmus <andrew.r.eras@gmail.com>
2023-11-21 10:22:08 +0200

    Add drag to paint feature

--- script.js
+++ script.js
@@ -2,32 +2,55 @@
 
 let container = document.getElementById('sketch-container');
 var computedStyles = window.getComputedStyle(container);
-
 const height = computedStyles.getPropertyValue('height');
 const width = computedStyles.getPropertyValue('width');
-
 var numericHeight = parseFloat(height);
 var numericWidth = parseFloat(width);
-
-
 let squareH = numericHeight / 16;
 let squareW = numericWidth / 16;
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.classList.add("square");
-    square.style.width = squareW + 'px';
-    square.style.height = squareH + 'px';
-    container.appendChild(square);
-    square.addEventListener('mouseover', (e)=>{
-        square.style.backgroundColor = 'black';
-    });
+
+loadCanvas();
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function loadCanvas() {
+    for (let i = 0; i < 256; i++) {
+        let square = document.createElement('div');
+        square.classList.add("square");
+        square.id = i;
+        square.style.width = squareW + 'px';
+        square.style.height = squareH + 'px';
+
+        container.appendChild(square);
+        square.addEventListener('dragstart', function(event) {
+            // Prevent the default drag-and-drop behavior
+            event.preventDefault();
+        });
+        square.addEventListener('mousedown', changeColor);
+        square.addEventListener('mouseover', changeColor);
+        
+
+
+    }
+}
+
+//Changes their color when dragged over
+function changeColor(e) {
+
+    if (e.type === 'mouseover' && mouseDown === true){
+    console.log(mouseDown);
+    e.target.style.backgroundColor = "black";
+    }
 }
 
 
-function clearCanvas(){
-    let squares=document.querySelectorAll(".square");
+// Clears the canvas when button clicked
+function clearCanvas() {
+    let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square)=>{
-        square.style.backgroundColor="white";
+    squares.forEach((square) => {
+        square.style.backgroundColor = "white";
     });
 }
